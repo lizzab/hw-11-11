6244be807bf06368802a4de067c56c0a84d0e59e
Lichtmanh <Lichtmanh@hartwick.edu>
2019-11-11 21:41:40 -0500

    Update hw-11-11.py

--- hw-11-11.py
+++ hw-11-11.py
@@ -4,5 +4,6 @@
 from Trucks import Trucks
 
 # Include color, company, model, year, doors
+# you need to instantiate the truck you wish to create before calling the description method
 
 description('silver', 'ram', 2500, 2009, 4)
