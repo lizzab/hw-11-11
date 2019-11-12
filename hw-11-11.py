e30087b89fcdd0fc801a3ee1476dbae74e3cce64
Howard Lichtman <Lichtmanh@hartwick.edu>
2019-11-12 08:48:04 -0500

    I modified the program where you create the Truck, you had the wrong line of code to create it.  I then called the description method to print the results.

--- hw-11-11.py
+++ hw-11-11.py
@@ -6,5 +6,6 @@ from Trucks import Trucks
 # Include color, company, model, year, doors
 # you need to instantiate the truck you wish to create before calling the description method
 
-car = Trucks.description('Silver', 'Ram', 2500, 2010, 4)
+car = Trucks('Silver', 'Ram', 2500, 2010, 4)
+car.description()
 
